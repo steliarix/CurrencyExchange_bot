fb8778fe1a1ad8a1398c24e4104af4d204231da6
Artem <stasiuk.artempy@gmail.com>
2021-02-16 15:32:23 +0200

    improve code quality

--- bot.py
+++ bot.py
@@ -2,23 +2,22 @@ import matplotlib.pyplot as plt
 import datetime as DT
 import telebot
 import requests
-from exchangeratesapi import Api
 from time import time
 import io
 import os
 
-api = Api()
-bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
+bot = telebot.TeleBot(os.environ["TELEGRAM_TOKEN"])
 
 storage = {}
 last_invocated = {}
 
 
-# Декоратор для кэширования
-
 def cached(ttl_s: int):
-    def cached_decorator(function):
+    """
+    Декоратор для кэширования
+    """
 
+    def cached_decorator(function):
         def cached_function(*args, **kwargs):
             key = f"{function.__name__}|{str(args)}|{str(kwargs)}"
 
@@ -39,69 +38,58 @@ def cached(ttl_s: int):
     return cached_decorator
 
 
-# Команда /help для боллее детальной информации о командах
-
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=["help"])
 def help_list(message):
-    bot.send_message(message.chat.id, f'/list - Выводит список курса валют USD \n /exchange - /exchange 10 USD to RUB (конвертация валюты из доллара в руб по '
-                                      f'курсу) \n /history - /history USD/RUB (График курса за последние 7 дней)')
+    """
+    Команда /help для боллее детальной информации о командах
+    """
+    bot.send_message(
+        message.chat.id,
+        f"/list - Выводит список курса валют USD \n/exchange - /exchange 10 USD to RUB (конвертация валюты из доллара в руб по "
+        f"курсу) \n/history - /history USD/RUB (График курса за последние 7 дней)",
+    )
 
 
-# Команда /list для списка курса валют USD
-
-@bot.message_handler(commands=['list'])
+@bot.message_handler(commands=["list"])
 def list_message(message):
-    response = cached(ttl_s=600)(api.get_rates)('USD')['rates']
-    text = ''
-    for k, v in response.items():
-        text += f'{k}: {round(v, 2)}\n'
-
+    """
+    Команда /list для списка курса валют USD
+    """
+    response = cached(ttl_s=600)(requests.get)("https://api.exchangeratesapi.io/latest?base=USD").json()["rates"]
+    text = "\n".join(f"{currency}: {amount:.2f}" for currency, amount in response.items())
     bot.send_message(message.chat.id, text)
 
 
-# Команда /exchange (/exchange 10 USD to RUB), конвертация валюты из доллара
-
-@bot.message_handler(commands=['exchange'], content_types=['text'])
+@bot.message_handler(commands=["exchange"], content_types=["text"])
 def list_exchange(message):
-    response = cached(ttl_s=600)(api.get_rates)('USD')['rates']
-    user_message = message.text
-    key_exchange = user_message[-3:]
-    number_in_message = [int(s) for s in user_message.split() if s.isdigit()]
-    i = sum(number_in_message)
-    try:
-        if response[key_exchange]:
-            bot.send_message(message.chat.id, f'{round(response[key_exchange], 3) * i} {key_exchange}')
-    except KeyError:
-        pass
+    """
+    Команда /exchange (/exchange 10 USD to RUB), конвертация валюты из доллара
+    """
+    response = cached(ttl_s=600)(requests.get)("https://api.exchangeratesapi.io/latest?base=USD").json()["rates"]
+    currency = message.text[-3:]
+    if currency in response:
+        amount = sum([int(s) for s in message.text.split() if s.isdigit()])
+        bot.send_message(message.chat.id, f"{response[currency] * amount:.2f} {currency}")
 
 
-# Команда /history (/history USD/RUB) для показа графика курса за последние 7 дней
-
-@bot.message_handler(commands=['history'], content_types=['text'])
+@bot.message_handler(commands=["history"], content_types=["text"])
 def list_exchange(message):
+    """
+    Команда /history (/history USD/RUB) для показа графика курса за последние 7 дней
+    """
     today = DT.date.today()
     week_ago = today - DT.timedelta(days=7)
-    user_message = message.text
-    key_exchange = user_message[-3:]
-    url = f'https://api.exchangeratesapi.io/history?start_at={week_ago}&end_at={today}&base=USD&symbols={key_exchange}'
+    currency = message.text[-3:]
     try:
-        res = requests.get(url).json()['rates'].values()
-        coords = []
-        for i in res:
-            coords += i.values()
-        week = coords
-        plt.plot(week)
-        fig = plt.figure()
-        plt.plot(week)
-        file = io.BytesIO()
-        fig.savefig(file)
-        file.seek(0)
-        bot.send_photo(message.chat.id, file)
-
-    except RuntimeError:
-        pass
+        rates = requests.get(f"https://api.exchangeratesapi.io/history?start_at={week_ago}&end_at={today}&base=USD&symbols={currency}").json()["rates"].values()
     except KeyError:
-        pass
+        return
+    coords = [exchange_rate for rate in rates for exchange_rate in rate.values()]
+    fig = plt.figure()
+    plt.plot(coords)
+    fig.savefig(file := io.BytesIO())
+    file.seek(0)
+    bot.send_photo(message.chat.id, file)
 
 
 bot.polling()
